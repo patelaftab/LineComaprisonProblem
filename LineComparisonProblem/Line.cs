627f8bcb52df2b97efe3dde7a1e4758c0bf9bc01
patelaftab <10patelaftab@gmail.com>
2023-02-05 22:24:12 +0530

    [Aftab] :Checking Equality Of Two Line

--- LineComparisonProblem/Line.cs
+++ LineComparisonProblem/Line.cs
@@ -17,11 +17,12 @@ namespace LineComparisonProblem
             this.pointY2 = pointY2;
         }
 
-        public void Length()
+        public double Length()
         {
             double length = Math.Sqrt(Math.Pow((poIntX2 - PointX1), 2) + Math.Pow((pointY2 - _pointY1), 2));
-            Console.WriteLine("length of line : " + length);
+            return length;
         }
+
 
     }
 }
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -10,9 +10,23 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-
             Line line = new Line(1, 2, 3, 4);
-            line.Length();
+            double length1 = line.Length();
+            Console.WriteLine("Length of 1st Line : " + length1);
+
+            Line line2 = new Line(5, 6, 7, 9);
+            double length2 = line2.Length();
+            Console.WriteLine("Length of 2nd Line : " + length2);
+
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("Both Line Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Both Line Are not Equal");
+            }
+
             Console.ReadLine();
         }
     }
