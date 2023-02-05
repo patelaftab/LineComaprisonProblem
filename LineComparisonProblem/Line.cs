cd71031bd2ab61a5375a5d8ca3f898760c66bb08
patelaftab <10patelaftab@gmail.com>
2023-02-05 22:17:45 +0530

    [Aftab] : Calculating Length Of a Line

---
+++ LineComparisonProblem/Line.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class Line
+    {
+        int PointX1, _pointY1, poIntX2, pointY2;
+        public Line(int pointX1, int pointY1, int pointX2, int pointY2)
+        {
+            this.PointX1 = pointX1;
+            _pointY1 = pointY1;
+            poIntX2 = pointX2;
+            this.pointY2 = pointY2;
+        }
+
+        public void Length()
+        {
+            double length = Math.Sqrt(Math.Pow((poIntX2 - PointX1), 2) + Math.Pow((pointY2 - _pointY1), 2));
+            Console.WriteLine("length of line : " + length);
+        }
+
+    }
+}
---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Line line = new Line(1, 2, 3, 4);
+            line.Length();
+            Console.ReadLine();
+        }
+    }
+}
