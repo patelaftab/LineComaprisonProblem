14c174231563ee23bcffde3f5ebbbf262ded4a5a
patelaftab <10patelaftab@gmail.com>
2023-02-15 13:27:10 +0530

    [Aftab] : Comapairing Two Lines Using Comapre To Method

---
+++ LineComparisonProblem/Compairing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class Compairing
+    {
+        public void LineComparison()
+        {
+            Line line = new Line(1, 2, 3, 5);
+            double length1 = line.Length();
+            Console.WriteLine("Length of 1st Line : " + length1);
+
+            Line line2 = new Line(1, 2, 3, 4);
+            double length2 = line2.Length();
+            Console.WriteLine("Length of 2nd Line : " + length2);
+
+            if (length1.CompareTo(length2) == 0)
+            {
+                Console.WriteLine("Both Line Are Equal");
+            }
+            else if (length1.CompareTo(length2) > 0)
+            {
+                Console.WriteLine("1st Line is greter");
+            }
+            else
+            {
+                Console.WriteLine("2nd Line is greter");
+            }
+        }
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -10,27 +10,8 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            Line line = new Line(1, 2, 3, 5);
-            double length1 = line.Length();
-            Console.WriteLine("Length of 1st Line : " + length1);
-
-            Line line2 = new Line(1, 2, 3, 4);
-            double length2 = line2.Length();
-            Console.WriteLine("Length of 2nd Line : " + length2);
-
-            if (length1.CompareTo(length2) == 0)
-            {
-                Console.WriteLine("Both Line Are Equal");
-            }
-            else if (length1.CompareTo(length2) > 0)
-            {
-                Console.WriteLine("1st Line is greter");
-            }
-            else
-            {
-                Console.WriteLine("2nd Line is greter");
-            }
-
+           Compairing compairing = new Compairing();
+            compairing.LineComparison();
             Console.ReadLine();
         }
     }
